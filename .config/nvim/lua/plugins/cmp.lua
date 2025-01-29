83562ea38166238a26e3f1d2e781e4b66319ef1b
Jonathan Kocevar <55568915+staticvalley@users.noreply.github.com>
2025-01-29 15:47:45 -0500

    fixed nvim formatters, customized autocomplete

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -4,78 +4,149 @@
 -- https://github.com/hrsh7th/nvim-cmp/tree/main
 
 return {
-  { 
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
       -- snippet engine
       {
-        'L3MON4D3/LuaSnip',
+        "L3MON4D3/LuaSnip",
         build = (function()
           -- conditional for regex support in snippets
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
             return
           end
-          return 'make install_jsregexp'
+          return "make install_jsregexp"
         end)(),
         dependencies = {
           {
-            'rafamadriz/friendly-snippets',
+            "rafamadriz/friendly-snippets",
             config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
+              require("luasnip.loaders.from_vscode").lazy_load()
             end,
           },
         },
       },
-      'saadparwaiz1/cmp_luasnip',
+      "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind-nvim",
 
       -- add other nvim-cmp repos
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
     },
     config = function()
       -- see `:help cmp`
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      luasnip.config.setup({})
 
-      cmp.setup {
+      local lspkind = require("lspkind")
+      local kind_icons = {
+        Text = "  ",
+        Method = "  ",
+        Function = "  ",
+        Constructor = "  ",
+        Field = "  ",
+        Variable = "  ",
+        Class = "  ",
+        Interface = "  ",
+        Module = "  ",
+        Property = "  ",
+        Unit = "  ",
+        Value = "  ",
+        Enum = "  ",
+        Keyword = "  ",
+        Snippet = "  ",
+        Color = "  ",
+        File = "  ",
+        Reference = "  ",
+        Folder = "  ",
+        EnumMember = "  ",
+        Constant = "  ",
+        Struct = "  ",
+        Event = "  ",
+        Operator = "  ",
+        TypeParameter = "  ",
+      }
+
+      cmp.setup({
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = { completeopt = "menu,menuone,noinsert" },
+        window = {
+          completion = cmp.config.window.bordered({
+            border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+            winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:PmenuSel,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+            scrollbar = false,
+            scrolloff = 10,
+          }),
+          documentation = cmp.config.window.bordered({
+            border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+            winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:PmenuSel,Search:None",
+          }),
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            -- quick fix for the snippet indicator added to tokens
+            if vim_item.abbr:sub(-1) == "~" then
+              vim_item.abbr = vim_item.abbr:sub(1, -2)
+            end
 
-        mapping = cmp.mapping.preset.insert {
+            -- trim token if too long
+            local abbr_max_len = 20
+            if #vim_item.abbr > abbr_max_len + 1 then
+              vim_item.abbr = vim.fn.strcharpart(vim_item.abbr, 0, abbr_max_len) .. "..."
+            end
+
+            -- add icons and menu
+            vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+            vim_item.menu = ({
+              buffer = "[Buffer]",
+              nvim_lsp = "[LSP]",
+              luasnip = "[LuaSnip]",
+              nvim_lua = "[Lua]",
+              latex_symbols = "[LaTeX]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+        experimental = {
+          ghost_text = false,
+        },
+        mapping = cmp.mapping.preset.insert({
           -- see `:help ins-completion`
 
           -- select next item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
           -- select previous item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
 
           -- scroll documentation display
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
           -- accept autocompletion
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ["<C-Enter>"] = cmp.mapping.confirm({ select = true }),
 
           -- manually trigger completion
-          ['<C-Space>'] = cmp.mapping.complete {},
-        },
+          ["<C-Space>"] = cmp.mapping.complete({}),
+        }),
         sources = {
           {
-            name = 'lazydev',
+            name = "lazydev",
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
             group_index = 0,
           },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
         },
-      }
+      })
     end,
   },
 }
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -5,17 +5,17 @@
 
 return {
   {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     keys = {
       {
-        '<leader>f',
+        "<leader>f",
         function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
+          require("conform").format({ async = true, lsp_format = "fallback" })
         end,
-        mode = '',
-        desc = 'format current buffer',
+        mode = "",
+        desc = "format current buffer",
       },
     },
     opts = {
@@ -27,22 +27,27 @@ return {
         local disable_filetypes = { c = true, cpp = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
+          lsp_format_opt = "never"
         else
-          lsp_format_opt = 'fallback'
+          lsp_format_opt = "fallback"
         end
         return {
           timeout_ms = 500,
           lsp_format = lsp_format_opt,
         }
       end,
+      formatters = {
+        stylua = {
+          prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+        },
+      },
       formatters_by_ft = {
-        lua = { 'stylua' },
+        lua = { "stylua" },
         javascript = { "prettier" },
-        rust = { 'rustfmt' }, 
-        c = { 'clang-format' },
-        cpp = { 'clang-format' },
-        sh = { 'shfmt' },
+        rust = { "rustfmt" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        sh = { "shfmt" },
       },
     },
   },
